6afba5d337ca37b8da2b72d225f477215914eb8f
Vishnu Priya <vpriya@autointelli.com>
2025-08-04 14:54:20 +0530

    added image logic

--- Utils/cloudinary.js
+++ Utils/cloudinary.js
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 cloudinary.config({
-  cloud_name: "dajorp2fh",
-  api_key: "592191868728841",
-  api_secret: "4E1UM0JgS_uqpiFBgKWP6EVdh8k"
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 export default cloudinary;
